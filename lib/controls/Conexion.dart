863879fa00de208f7b5f6b9f14c60d7f66b36149
Jhandry <jhandrychimbo@gmail.com>
2024-02-10 19:10:05 -0500

    Actualizacion Conexion

--- lib/controls/Conexion.dart
+++ lib/controls/Conexion.dart
@@ -6,12 +6,9 @@ import 'package:noticias/controls/utiles/Utiles.dart';
 
 class Conexion {
   // CASA
-  final String URL = "http://192.168.0.104:3000/api/";
+  final String URL = "http://192.168.0.105:3000/api/";
   final String URL_MEDIA = "http://192.168.0.105:3000/api/images/";
 
-  // UNIVERSIDAD
-  // final String URL = "http://10.20.138.88:3000/api/";
-  // final String URL_MEDIA = "http://10.20.138.88:3000/api/images/";
   static bool NO_TOKEN = false;
   //anime-token
   Future<RespuestaGenerica> solicitudGet(String recurso, bool token) async {
@@ -107,80 +104,6 @@ class Conexion {
         uri,
         headers: _header,
         body: jsonEncode(mapa),
-      );
-
-      if (response.statusCode != 200) {
-        if (response.statusCode == 404) {
-          return _response(404, "Recurso no encontrado", []);
-        } else {
-          Map<dynamic, dynamic> mapa = jsonDecode(response.body);
-          return _response(mapa['code'], mapa['msg'], mapa['datos']);
-        }
-      } else {
-        Map<dynamic, dynamic> mapa = jsonDecode(response.body);
-        return _response(mapa['code'], mapa['msg'], mapa['datos']);
-      }
-    } catch (e) {
-      return _response(500, "Error inesperado", []);
-    }
-  }
-
-  Future<RespuestaGenerica> solicitudPutBool(
-      String recurso, bool token, Map<dynamic, dynamic> mapa) async {
-    Map<String, String> _header = {'Content-Type': 'application/json'};
-    if (token) {
-      Utiles util = Utiles();
-      String? token = await util.getValue('token');
-      _header = {
-        'Content-Type': 'application/json',
-        'anime-token': token.toString(),
-      };
-    }
-
-    final String _url = URL + recurso;
-    final uri = Uri.parse(_url);
-
-    try {
-      final response = await http.put(
-        uri,
-        headers: _header,
-        body: jsonEncode(mapa),
-      );
-
-      if (response.statusCode != 200) {
-        if (response.statusCode == 404) {
-          return _response(404, "Recurso no encontrado", []);
-        } else {
-          Map<dynamic, dynamic> mapa = jsonDecode(response.body);
-          return _response(mapa['code'], mapa['msg'], mapa['datos']);
-        }
-      } else {
-        Map<dynamic, dynamic> mapa = jsonDecode(response.body);
-        return _response(mapa['code'], mapa['msg'], mapa['datos']);
-      }
-    } catch (e) {
-      return _response(500, "Error inesperado", []);
-    }
-  }
-
-  Future<RespuestaGenerica> solicitudPutVoid(String recurso, bool token) async {
-    Map<String, String> _header = {'Content-Type': 'application/json'};
-    if (token) {
-      Utiles util = Utiles();
-      String? token = await util.getValue('token');
-      _header = {
-        'Content-Type': 'application/json',
-        'token': token.toString(),
-      };
-    }
-
-    final String _url = URL + recurso;
-    final uri = Uri.parse(_url);
-
-    try {
-      final response = await http.put(
-        uri,
-        headers: _header,
       );
 
       if (response.statusCode != 200) {
